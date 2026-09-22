4bf0349f188b53a3ef196e1d38e443d9fbb59024
Bobur Akh <boburakhone@gmail.com>
2026-09-22 16:52:29 +0500

    some changes on the UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
+    <header className="app-header">
+      <img src="js-two.png" alt="Javascript logo" />
+      <h1>The Happy Quiz</h1>
     </header>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,9 +3,9 @@ import React from "react";
 function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to The React Quiz!</h2>
+      <h2>Welcome to The Happy Quiz!</h2>
 
-      <h3>{numQuestions} questions to test your React mastery</h3>
+      <h3>{numQuestions} questions to test your Javascript mastery</h3>
       {numQuestions > 0 && (
         <button
           className="btn btn-ui"
